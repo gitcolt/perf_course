49ee4998e669e18cae6277e5cbae63ec019faa78
Colten Judice <coltjobhunt@protonmail.com>
2024-04-22 03:56:33 -0500

    Redo haversine json input generator

--- haversine/main.c
+++ haversine/main.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 #include "haversine.h"
-#include "json_gen.h"
 
 #define LAT_MAX 90
 #define LON_MAX 180
+
+#define EARTH_RADIUS 6372.8
 
 f64 rand_lat() {
     return (f64)rand() / ((f64)RAND_MAX/(LAT_MAX * 2)) - LAT_MAX;
@@ -26,23 +27,27 @@ int main(int argc, char *argv[]) {
 
     srand(time(NULL));
 
-    jg_start();
-    jg_arr_start("pairs");
+    f64 sum = 0;
+    f64 sum_coef = 1.0 / (f64)num_pairs;
+
+    printf("{\"pairs\": [\n");
     for (int i = 0; i < num_pairs; ++i) { 
-        jg_obj_start(NULL);
-        jg_obj_key_val_float("x0", rand_lat());
-        jg_obj_key_val_float("y0", rand_lon());
-        jg_obj_key_val_float("x1", rand_lat());
-        jg_obj_key_val_float("y1", rand_lon());
-        jg_obj_end();
+        f64 x0 = rand_lon();
+        f64 y0 = rand_lat();
+        f64 x1 = rand_lon();
+        f64 y1 = rand_lat();
+        printf("    { \"x0\": %11f, \"y0\": %11f, \"x1\": %11f, \"y1\": %11f }%s\n",
+                x0,
+                y0,
+                x1,
+                y1,
+                i == num_pairs - 1 ? "" : ",");
+
+        f64 haversine_dist = ReferenceHaversine(x0, y0, x1, y1, EARTH_RADIUS);
+        sum += sum_coef * haversine_dist;
     }
-    jg_arr_end();
+    printf("]}\n");
 
-    jg_end();
-
-    jg_print();
-    // const char *filename = "out.json";
-    // jg_write_to_file(filename);
-
-    jg_destroy();
+    printf("\n");
+    printf("sum: %f\n", sum);
 }
